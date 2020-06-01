abf3c5d4487453b37506903f242da8a9a6b35b9a
Otavio Ornellas <Otavio Ornellas>
2020-06-01 01:26:29 -0300

    Added navbar, side menu, fixed layout, add fonts by fontello

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavigationBarComponent,
+    SideMenuComponent
   ],
   imports: [
     BrowserModule,
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { imageSource } from 'src/environments/imageSource';
+import UIkit from 'uikit';
 
 @Component({
   selector: 'app-home',
@@ -8,21 +9,14 @@ import { imageSource } from 'src/environments/imageSource';
 })
 export class HomeComponent implements OnInit {
 
+
+
   imageSource: string[];
 
   constructor() {
     this.imageSource = imageSource;
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
-  minhaFuncao(img) {
-    return {
-      'background-image': `url(${img})`,
-      height: '100%',
-      width: '100%',
-      'background-size': 'cover'
-    };
-  }
 }
